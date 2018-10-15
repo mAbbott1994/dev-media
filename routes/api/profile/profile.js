96722f16fb36f5b4c53f1f4d21542efb385c5700
Michael <mabbott1994@icloud.com>
2018-10-15 08:09:57 +0100

    get user by ID profile route

--- routes/api/profile/profile.js
+++ routes/api/profile/profile.js
@@ -1,11 +1,29 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const validateProfileInput = require("../../../middleware/rules/profile");
+const passport = require("passport");
 const User = require("../../../models/User");
-const passport = require('passport');
 
-
-// @route   POST api/profile
-// @desc    Create or edit user profile
+// @route   get api/profile/:User_id
+// @desc    Get user by id
 // @access  Private
 
+router.get("/:User_id", (req, res) => {
+  const { errors, isValid } = validateProfileInput(req.body);
+  const profileUpdates = {};
+
+  User.findById({ user: req.params.User_id })
+    .then(profile => {
+      if (!profile) {
+        errors.profile = "Profile not found";
+        return res.status(404).json(errors);
+      }
+      return res.json(profile);
+    })
+    .catch(err => {
+      return res.status(404).json({
+        profile: "No profile found for this user"
+      });
+    });
+});
 
 module.exports = router;
