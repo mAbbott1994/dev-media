e27df281100a24866c65cebd10482341da2555b2
michael <mabbott1994@icloud.com>
2018-11-24 18:50:19 +0000

    Added routes toe add comments and delete comments based on req.params

--- routes/api/posts/posts.js
+++ routes/api/posts/posts.js
@@ -112,7 +112,6 @@ router.delete(
  * @desc Like post
  * @access private
  */
-
 router.post(
 	"/like/:id",
 	passport.authenticate("jwt", { session: false }),
@@ -123,9 +122,7 @@ router.post(
 					post.likes.filter(like => like.user.toString() === req.user.id)
 						.length > 0
 				) {
-					return res
-						.status(400)
-						.json({ liked: "User already liked this post" });
+					res.status(400).json({ liked: "User already liked this post" });
 				}
 
 				post.likes.unshift({ user: req.user.id });
@@ -164,6 +161,87 @@ router.post(
 				post.save().then(post => res.status(200).json({ success: true, post }));
 			});
 		});
+	}
+);
+
+/**
+ * @route POST /api/posts/comment/:id
+ * @desc Add comment to  post
+ * @access private
+ */
+
+router.post(
+	"/comment/:id",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		const { errors, isValid } = validatePostInput(req.body);
+
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
+
+		Post.findById(req.params.id)
+			.then(post => {
+				const newComment = {
+					text: req.body.text,
+					name: req.body.name,
+					avatar: req.body.avatar,
+					user: req.user.id
+				};
+
+				post.comments.unshift(newComment);
+
+				post
+					.save()
+					.then(post => res.status(200).json(post))
+					.catch(err =>
+						res.status(404).json({ postnotfound: "no post was found" })
+					);
+			})
+			.catch(() =>
+				res
+					.status(400)
+					.json({ commenterror: "Couldn't create comment, try again later" })
+			);
+	}
+);
+
+/**
+ * @route DELETE /api/posts/comment/:id/:comment_id
+ * @desc delete comment from  post
+ * @access private
+ */
+
+router.delete(
+	"/comment/:id/:comment_id",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		Post.findById(req.params.id)
+			.then(post => {
+				if (
+					post.comments.filter(
+						comment => comment._id.toString() === req.params.comment_id
+					).length === 0
+				) {
+					return res
+						.status(404)
+						.json({ commentnotfound: "Comment doesn't exist" });
+				}
+				const removeIndex = posts.comments
+					.map(item => item._id.toString)
+					.indexOf(req.params.comment_id);
+
+				post.comment.splice(removeIndex, 1);
+
+				posts
+					.save()
+					.then(post => res.status(200).json({ success: true, post }));
+			})
+			.catch(() =>
+				res
+					.status(400)
+					.json({ commenterror: "Couldn't create comment, try again later" })
+			);
 	}
 );
 
